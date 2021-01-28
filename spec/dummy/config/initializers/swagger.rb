0e3db025298137634639ae523c2a868f15ee4fc1
Dmitry Sadovsky <dimailn9@gmail.com>
2021-01-28 16:14:03 +0300

    injected html option

--- spec/dummy/config/initializers/swagger.rb
+++ spec/dummy/config/initializers/swagger.rb
@@ -1,3 +1,4 @@
 GrapeSwaggerRails.options.url = '/api/swagger_doc'
 GrapeSwaggerRails.options.app_name = 'Swagger'
 GrapeSwaggerRails.options.app_url  = 'http://localhost:3000'
+GrapeSwaggerRails.options.injected_html  = ''
